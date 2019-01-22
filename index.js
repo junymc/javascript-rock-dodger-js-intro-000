00dfe2f540fc48c45822e4c133d69eb73acd9f1f
Junyoung McArdle <juny.mcardle@gmail.com>
2019-01-22 19:26:21 +0000

    Done.

--- index.js
+++ index.js
@@ -22,28 +22,32 @@ function checkCollision(rock) {
   // use the comments below to guide you!
   const top = positionToInteger(rock.style.top)
 
+
+
   // rocks are 20px high
   // DODGER is 20px high
   // GAME_HEIGHT - 20 - 20 = 360px;
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
-    // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
+    //  The DODGER is 40 pixels wide -- how do we get the right edge?
+    const dodgerRightEdge = positionToInteger(DODGER.style.left) + 40
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
-    // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
+    //  The rock is 20 pixel's wide -- how do we get the right edge?
+    const rockRightEdge = positionToInteger(rock.style.left) + 20
 
-    if (false /**
+    if ((rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) ||
+       (rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerRightEdge) ||
+       (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge) /**
                * Think about it -- what's happening here?
                * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
+               * true1. The rock's left edge is < the DODGER's left edge,
                *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
+               * true2. The rock's left edge is > the DODGER's left edge,
                *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
+               * false3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge
                */) {
       return true
@@ -73,6 +77,7 @@ function createRock(x) {
    * seems like a good pace.)
    */
   function moveRock() {
+
     // implement me!
     // (use the comments below to guide you!)
     /**
@@ -122,6 +127,7 @@ function moveDodger(e) {
 }
 
 function moveDodgerLeft() {
+
   // implement me!
   /**
    * This function should move DODGER to the left
